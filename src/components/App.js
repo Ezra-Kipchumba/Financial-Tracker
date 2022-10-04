6ba533719b7182445c4a74428f7ed2a04dba4393
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-04 14:05:32 +0300

    modify app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Balance from './Balance';
+import Expense from './Expense';
+import Header from './Header';
+import TransactionForm from './TransactionForm';
+import TransactionList from './TransactionList';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Balance />
+      <Header />
+      <TransactionForm />
+      <Expense />
+      <TransactionList />
     </div>
   );
 }
