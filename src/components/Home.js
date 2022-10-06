1e5ca9894b53b32a51fcb1855718ccc11e1f4264
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-07 00:05:20 +0300

    add assets folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-      <div>
-          <h1>Home Page</h1>
+    <div>
+      <h1 style={{fontWeight:"bold"}}>Welcome,</h1>
+      <h3>With us, we help you manage your finances <br></br>for a better tommorrow</h3>
     </div>
   )
 }
