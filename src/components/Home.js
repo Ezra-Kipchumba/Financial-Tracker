213a08153532e50cfa236422f1943c9743852d5e
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-07 00:26:17 +0300

    add images to assets

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,20 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      <h1 style={{fontWeight:"bold"}}>Welcome,</h1>
-      <h3>With us, we help you manage your finances <br></br>for a better tommorrow</h3>
+      <h1 style={{ fontWeight: "bold" }}>Welcome,</h1>
+      <h2>
+        With us, we help you manage your finances <br></br>for a better
+        tommorrow
+      </h2>
+      <div>
+        <img
+          src="../assets/micheile-dot-com-f030K9IzpcM-unsplash.jpg"
+          alt="saving"
+        ></img>
+        <img src="../assets/matthew-lancaster-OtfnlTw0lH4-unsplash.jpg" alt="finances"></img>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
